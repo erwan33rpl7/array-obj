81b79f508a41f662413914ed5cc696aae65336dd
ewraan septian nur R <erwan_ramadhan_33rpl@student.smktelkom-mlg.sch.id>
2024-11-01 11:12:10 +0700

    Add files via upload

---
+++ testerizz.java
@@ -0,0 +1,30 @@
+package penyewaan;
+
+public class testerizz {
+    public static void main(String[] args) {
+        String []nama= {"dafter nama yang ngawor: ","nama","nini","baba","mmmmm"};
+        for(int p=0;p<nama.length;p++){
+            System.out.println(nama[p]);
+        }//array object
+        // Product[]produk = new Product[10];
+        // CD[]c = new CD[5];
+        // produk[0]=new Product();
+        // produk[0].print();
+        // c[4]=new CD(1,"erwan",7,20000,"wewegombel",3,"ngawi");
+        // c[4].print();
+        
+        
+        DVD[]D=new DVD[10];
+        D[2]=new DVD(1,"erwan",3,3000,90,"PG15","ngewe");
+        D[2].print();
+
+
+
+
+
+
+
+
+
+    }
+}
